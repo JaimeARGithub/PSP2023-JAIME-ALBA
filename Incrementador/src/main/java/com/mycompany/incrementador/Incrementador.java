4a27cc35e9e7b96050dfe40de1015b6cd76736ec
Jaime Alba Ruiz <jalbar03@educastillalamancha.es>
2023-11-25 10:36:49 +0100

    25/11/2023
    Incrementador de clase corregido y funcional. Ejercicio 7 corregido y funcioanl, añadido un cambio de directorio. El 8 ya iba de antes.

--- Incrementador/src/main/java/com/mycompany/incrementador/Incrementador.java
+++ Incrementador/src/main/java/com/mycompany/incrementador/Incrementador.java
@@ -26,27 +26,114 @@ public class Incrementador {
         System.out.println("-------------------------");
         */
         
-        // Contador con hilos:
-        int valor = 0;
-        Contador c = new Contador(valor);
+        // Contador con hilos: programa principal
+        // 1.- Crea una instancia del recurso compartido
+        Contador c = new Contador(0); 
         
-        Thread h1 = new Thread(c);
-        Thread h2 = new Thread(c);
-        Thread h3 = new Thread(c);
-        Thread h4 = new Thread(c);
+        // 2.- Nos creamos un array de hilos, para controlar qué
+        // hilo se está ejecutando en cada momento
+        int numeroHilos = 10;
+        Thread[] hilos = new Thread[numeroHilos];
         
-        h1.start();
-        h2.start();
-        h3.start();
-        h4.start();
+        for (int i=0; i<numeroHilos; i++) {
         
+        // 3.- Se crean y ejecutan    
+            
+            // Creamos una instancia de la clase sumador; por sí sola no hace nada
+            Sumador sum = new Sumador(c,i);
+            
+            // Creamos un hilo para ese sumador y lo guardamos en una posición
+            // del array de hilos
+            hilos[i] = new Thread(sum); // Ésto todavía no hace nada
+            hilos[i].start();
+        }
+        
+        
+        
+        // 4.- Necesitamos otro bucle que, para cada uno de los hilos, el
+        // hilo principal espere a que finalicen
+        
+        // Con ésto, el programa principal espera a que terminen los hilos,
+        // pero sigue habiendo descuadre en la suma.
+        // Problema de la sección crítica
+        // Podemos añadir al método un modificador para indicar que sólo puede
+        // haber UN HILO a la vez ejecutando sumar. Me voy al método Sumar 
+        // de la clase Contador y le meto un synchronized.
+        
+        // Con ello, los hilos siguen ejecutándose a su aire, pero ya no puede
+        // haber dos operando a la vez con el mismo contenido de la memoria.
+        // La zona de memoria se bloquea; un hilo no puede trabajar con ella
+        // si ya hay otro hilo trabajando con ella
+        
+        // Problemas: 
+        // 1.>> Sincronizar un método RALENTIZA EL FUNCIONAMIENTO. Hay
+        // que saber qué métodos sincronizar y cuáles no.
+        // 2.>> Si tengo varios métodos sincronizados, como un sumar y
+        // restar, puede ocurrir un interbloqueo. Comprobar que el orden
+        // de ejecución de hilos no te va a provocar ésto. Ej: para que un
+        // hilo complete una tarea necesita acceder dos archivos; el hilo
+        // 1 usa el archivo 1 y necesita el 2, y el hilo 2 usa el archivo
+        // 2 y necesita el 1. El uno necesita el otro sin soltar el suyo
+        // y se quedan bloqueados.
+        for (int i=0; i<numeroHilos; i++) {
+            try {
+                hilos[i].join();
+            } catch (InterruptedException e) {
+                System.out.println("Excepción de tipo interrupted");
+            }
+        }
+        
+        
+        
+        
+        // Queremos que al acabar se muestre la cuenta obtenida
+        // El programa principal debe mostrar el resultado
+        System.out.printf("El resultado de la cuenta incrementado es %d \n", c.getCuenta());
     }
 }
 
 
 
+// Clase que implementa el hilo
+class Sumador implements Runnable {
+    // Este hilo necesita, como mínimo, un ID.
+    // String de nombre o nº que lo identifique
+    
+    // Le voy a decir qué hilo es y pasarle una instancia de la clase Contador
+    private int numHilo; // El identificador del hilo
+    
+    // ES FINAL PORQUE ES LA MISMA INSTANCIA PARA TODOS LOS HILOS.
+    // TODOS LOS HILOS TIENEN EL MISMO CONTADOR
+    private final Contador c; // Instancia del recurso compartido, de la clase contador
+    
+    public Sumador(Contador c, int num) {
+        this.numHilo = num;
+        this.c = c;
+    }
+    
+    
+    // El método run se va a llamar igual, pero pueden realizar distintas
+    // cosas dependiendo de los parámetros que se le hayan pasado al constructor
+    @Override
+    // El run() cambia según el hilo.
+    public void run() {
+        // Es recomendable poner a cada hilo un system out que los
+        // identifique cuando trabajen.
+        System.out.println(this.numHilo + " iniciado.");
+        for (int i=0; i<10000; i++) {
+            c.Sumar(); // Se ejecuta el método incrementador 100 veces
+        }
+        System.out.println(this.numHilo + " terminado.");
+    }
+    
+}
 
-class Contador implements Runnable {
+
+
+// El programa principal que inctanca la clase compartida y lanza los hilos
+// Se trata del recurso compartido, la clase con la que van a tratar todos
+// los hilos (un objeto de esa clase)
+class Contador{
     private int cuenta;
     
     
@@ -55,19 +142,11 @@ class Contador implements Runnable {
     }
     
     
-    public void incrementa() {
-        for (int i = 0; i<50000; i++) {
+    public synchronized void Sumar() {
             this.cuenta++;
-        }
     }
     
     public int getCuenta() {
         return this.cuenta;
-    }
-
-    @Override
-    public void run() {
-        incrementa();
-        System.out.printf("Hilo %d: El resultado final es %d\n", Thread.currentThread().getId(), getCuenta());
     }
 }
--- Tema1Ej7/src/main/java/com/mycompany/tema1ej7/Tema1Ej7.java
+++ Tema1Ej7/src/main/java/com/mycompany/tema1ej7/Tema1Ej7.java
@@ -19,23 +19,43 @@ public class Tema1Ej7 {
 
     public static void main(String[] args) {
         if (args.length!=3) {
-            System.out.println("Son necesarios tres argumentos.");
+            System.out.println("Error, debe introducir tres parámetros: archivo de búsqueda, patrón y archivo de salida.");
             System.exit(0);
         }
         
-        String entrada = args[0];
-        String pattern = args[1];
-        String salida = args[2];
+        
+        String dirBase = new String("/home/jaime/Descargas/");
+        
+        String entrada = new String(dirBase.concat(args[0]));
+        String pattern = args[1]; // Recogemos el patrón de búsqueda
+        String salida = new String(dirBase.concat(args[2])); // Recogemos el archivo de salida
         
         File archEnt = new File(entrada);
         
-        if (!archEnt.exists()) {
-            System.out.println("No existe el archivo que se intenta usar como entrada.");
+        if (!archEnt.exists() || archEnt.isDirectory()) {
+            System.out.println("Error; el archivo no existe o es un directorio.");
             System.exit(1);
         }
         
-        ProcessBuilder pb = new ProcessBuilder("grep", pattern, entrada);
+        ProcessBuilder pb = new ProcessBuilder("grep", pattern, args[0]);
         File fSalida = new File(salida);
+        
+        
+        // En el caso de que quiera indicarle en qué directorio buscar:
+        pb.directory(new File(dirBase));
+        
+        // Si nos vamos a andar moviendo de directorio, hay que tener en cuenta una
+        // cosa: a la hora de crear objetos File en Java, el parámetro no es el nombre
+        // del archivo, sino su ruta. Podemos crearnos una String con un cacho de
+        // ruta que luego sumarle al nombre del archivo o podemos sencillamente
+        // usarlo completo en todas partes.
+        // Para nuestro caso, mejor lo de crearnos una String con el trozo de ruta.
+        
+        // String dirBase = new String("/home/jaime"); y concatenárselo a todos
+        // los nombres de archivos
+        // También se pasa como parámetro al pb.directory
+        
+        
         pb.redirectOutput(fSalida);
         
         
